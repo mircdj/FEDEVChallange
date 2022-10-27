e6ce98f78f145e1a5969921324a8d9454369d441
A473665 <mirco.sabatino@enel.com>
2022-10-27 10:40:48 +0200

    add more quiz

--- core/challenge.test.js
+++ core/challenge.test.js
@@ -75,3 +75,11 @@ describe("Basic tests", () => {
     });
   });
   
+
+  //q9
+  describe("Tests", () => {
+    it("test", () => {
+      assert.strictEqual(checkCoupon('123','123','September 5, 2014','October 1, 2014'), true);
+      assert.strictEqual(checkCoupon('123a','123','September 5, 2014','October 1, 2014'), false);
+    });
+  });
---
+++ q8/q8.js
@@ -0,0 +1,26 @@
+/*
+
+Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+
+*/
+
+function reverseLetter(str) {
+    //your code here
+}
+
+console.log(reverseLetter("krishan"));
+console.log(reverseLetter("ultr53o?n"));
+console.log(reverseLetter("ab23c"));
+console.log(reverseLetter("krish21an"));
